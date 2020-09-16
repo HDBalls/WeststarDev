84aee5c29f88dea8049fcf149ec07591977236e0
ucheumeevuruo <ucheumeevuruo@yahoo.com>
2020-09-16 11:58:37 +0100

    Workshop

--- workshop/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import vehicles
-from . import project
-from . import vehicle_services
-# from . import product
--- workshop/wizards/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import create_work_order
-from . import project_create_sale_order
