9aa32f46413a510febc6d46490ab483aa49802de
ucheumeevuruo <ucheumeevuruo@yahoo.com>
2020-12-13 21:34:17 +0100

    Update sales.py

--- bluestar/models/sales.py
+++ bluestar/models/sales.py
@@ -10,6 +10,21 @@ class SaleOrder(models.Model):
 
     amount_undiscounted = fields.Monetary('Amount Before Discount', compute='_compute_amount_undiscounted', digits=0)
 
+    @api.onchange('partner_id')
+    def onchange_customer_id(self):
+        Template = self.env['sale.order.template']
+        template = False
+        name = self.partner_id.name
+        
+        if name:
+            if 'JULIUS BERGER' in name.upper():
+                template = Template.search([('name', 'ilike', 'Julius Berger')], limit = 1)
+            else:
+                template = Template.search([('name', 'ilike', 'Default')], limit = 1)
+        
+        if template != False:
+            self.update({'sale_order_template_id': template.id})
+
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     
