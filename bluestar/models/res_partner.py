a9c6f87b25decc6c08aeb1612e271ed0572a0197
ucheumeevuruo <ucheumeevuruo@yahoo.com>
2021-03-18 03:56:47 +0100

    Update res_partner.py

--- bluestar/models/res_partner.py
+++ bluestar/models/res_partner.py
@@ -13,7 +13,7 @@ class ResPartner(models.Model):
         if len(vals_list) == 1:
             customer_number = ''
             seq_date = None
-            if vals_list[0].get('supplier_rank', 1) == 1:
+            if vals_list[0].get('supplier_rank') == 1:
                 customer_number = self.env['ir.sequence'].next_by_code('bluestar.supplier.number.sequence', sequence_date=seq_date) or ''
             else:
                 customer_number = self.env['ir.sequence'].next_by_code('bluestar.customer.number.sequence', sequence_date=seq_date) or ''
