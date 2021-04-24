acb5b1d748a7e54968f274878a6379178b769bf8
ucheumeevuruo <ucheumeevuruo@yahoo.com>
2021-04-24 17:07:29 +0100

    Add to selection in pricelist

--- bluestar/models/__init__.py
+++ bluestar/models/__init__.py
@@ -4,3 +4,4 @@ from . import sales
 from . import product
 from . import account_asset
 from . import res_partner
+from . import pricelist
---
+++ bluestar/models/pricelist.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class PricelistItem(models.Model):
+    _inherit = 'product.pricelist.item'
+
+    base = fields.Selection(selection_add=[('gross_price', 'Base List Price')])
