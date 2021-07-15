9f739f0ec2470d9c6dba0607c4825483f88770b9
Chigozirim Ogbogu <cogbogu@plexada-si.com>
2021-07-15 23:24:50 +0100

    Update

--- workshop/__manifest__.py
+++ workshop/__manifest__.py
@@ -41,7 +41,7 @@
         'reports/workshop_account_invoice_3.xml',
         'reports/workshop_account_invoice.xml',
         'reports/workshop_workorder_invoice.xml',
-        'workshop_estimate_invoice_header_inherit_cr.xml',
+        'reports/workshop_estimate_invoice_header_inherit_cr.xml',
         'reports/report.xml'
     ],
     # only loaded in demonstration mode
