122fc2544f128fabe369ec1a96cf5187c68c8653
Chigozirim Ogbogu <cogbogu@plexada-si.com>
2021-07-31 12:11:45 +0100

    Update manifest.xml

--- bluestar_reports/__manifest__.py
+++ bluestar_reports/__manifest__.py
@@ -31,6 +31,7 @@
         'reports/report_proforma_invoice-JB.xml',
         'reports/account_report.xml',
         'reports/report_proforma_workshop.xml',
+        'reports/report_quotation_workshop.xml',
         'reports/report.xml'
     ],
     # only loaded in demonstration mode
